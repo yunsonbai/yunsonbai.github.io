7651acc68cdb5c50d90b26876bc455d2aae215c5
yunsonbai <yunsonbai@sohu-inc.com>
2018-10-29 16:07:19 +0800

    Site updated: 2018-10-29 16:07:18

--- js/src/ad.js
+++ js/src/ad.js
@@ -5,21 +5,21 @@ $(document).ready(function () {
         return
     }
 
-    ad = document.createElement("div");
-    ad_a = document.createElement("a");
-    // ad_a2 = document.createElement("a");
-    ad_a.href = "https://shop278649546.taobao.com/index.htm?spm=2013.1.w5002-14767040668.2.13561bce2HpOaU"
-    // ad_a2.href = "https://shop278649546.taobao.com/index.htm?spm=2013.1.w5002-14767040668.2.13561bce2HpOaU"
-    ad_a_img = document.createElement("img");
-    // ad_a_img2 = document.createElement("img");
-    ad_a_img.className = "ad"
-    // ad_a_img2.className = "ad"
-    // ad_a_img2.src = "https://img.alicdn.com/imgextra/i1/867684244/O1CN011hDnMfpRNKtDySv_!!867684244.jpg"
-    ad_a_img.src = "https://img.alicdn.com/imgextra/i3/867684244/O1CN011hDnMgSKJYpaOEX_!!867684244.jpg"
+    // ad = document.createElement("div");
+    // ad_a = document.createElement("a");
+    // // ad_a2 = document.createElement("a");
+    // ad_a.href = "https://shop278649546.taobao.com/index.htm?spm=2013.1.w5002-14767040668.2.13561bce2HpOaU"
+    // // ad_a2.href = "https://shop278649546.taobao.com/index.htm?spm=2013.1.w5002-14767040668.2.13561bce2HpOaU"
+    // ad_a_img = document.createElement("img");
+    // // ad_a_img2 = document.createElement("img");
+    // ad_a_img.className = "ad"
+    // // ad_a_img2.className = "ad"
+    // // ad_a_img2.src = "https://img.alicdn.com/imgextra/i1/867684244/O1CN011hDnMfpRNKtDySv_!!867684244.jpg"
+    // ad_a_img.src = "https://img.alicdn.com/imgextra/i3/867684244/O1CN011hDnMgSKJYpaOEX_!!867684244.jpg"
 
-    ad.className = "post-time"
-    ad_a.append(ad_a_img)
-    // ad_a.append(ad_a_img2)
-    ad.append(ad_a)
-    meta[0].append(ad)
+    // ad.className = "post-time"
+    // ad_a.append(ad_a_img)
+    // // ad_a.append(ad_a_img2)
+    // ad.append(ad_a)
+    // meta[0].append(ad)
 })
